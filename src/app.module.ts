1756fdf615078dd07f7a836f0c043fe3217a0de6
doogler777 <doogler777@gmail.com>
2021-11-18 21:11:01 +0900

    chore: 프로젝트 생성
    - eslintrc 파일 수정
    - .gitigore에 .env 추가
    - user인증관련 패키지 설치
    - TypeOrm 설치 및 설정
    - conifg모듈 설치 및 설정
    - class-valitor, class-transformer 설치 및 설정
    - moment 설치
    - mysql 설치
    resolves #1 #3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: process.env.DB,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      port: 3306,
+      password: process.env.DB_PASS,
+      entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
