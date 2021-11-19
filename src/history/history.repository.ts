13eda7cd8ddb0af0ed865bb5c65b9fff34a24882
lhj0621 <lhj0621@gmail.com>
2021-11-19 22:50:02 +0900

    feat : 반납 개발
    - 반납 API 개발
    - 이벤트 서비스 전달 데이터 가공
    - findOne 에러 처리
    - 이미 반납된 요청 에러 처리
    
    resolves: #11

--- src/history/history.repository.ts
+++ src/history/history.repository.ts
@@ -3,23 +3,41 @@ import { EntityRepository, IsNull, Not, Repository } from 'typeorm';
 import { UpdateHistroyDto } from './dto/update-history.dto';
 import { User } from '../entities/user.entity';
 import * as moment from 'moment-timezone';
-import { InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 
 @EntityRepository(History)
 export class HistoryRepository extends Repository<History> {
+  async historyFindOne(id: number, user: User): Promise<History> {
+    const history = await this.findOne({
+      history_id: id,
+      user,
+    });
+    if (!history) {
+      throw new NotFoundException('유효한 요청이 아닙니다.');
+    }
+    return history;
+  }
+
   async updateReturnHistory(
     id: number,
     updateHistroyDto: UpdateHistroyDto,
     user: User,
   ) {
-    const history = await this.findOne({
-      history_id: id,
-      user,
-    });
+    let sendData: any = {};
+    let price = 0;
+    const { latitude, longitude } = updateHistroyDto;
+
+    const use_end_at = new Date();
+    const history = await this.historyFindOne(id, user);
+    if (history.use_end_at) {
+      throw new BadRequestException('이미 반납된 요청입니다.');
+    }
 
     const { base_price, min_price } = history.deer.area;
-    let price = 0;
-    const use_end_at = new Date();
     const useMin = this.getTimeDiff(history.use_start_at, use_end_at);
 
     //사용시간 1분 이상일때
@@ -35,73 +53,45 @@ export class HistoryRepository extends Repository<History> {
     history.price = price;
     history.use_end_at = use_end_at;
 
+    const historyQuery = await this.createQueryBuilder('history')
+      .where('history_id = :id', { id })
+      .leftJoin('history.deer', 'deer')
+      .leftJoin('deer.area', 'area')
+      .leftJoin('area.parkingzones', 'parkingzone')
+      .leftJoin('area.forbidden_areas', 'forbidden_area')
+      .addSelect(
+        'SUM(ST_Contains(area.area_boundary, ST_GeomFromText(:p)))',
+        'isInArea',
+      )
+      .addSelect(
+        'SUM(ST_DISTANCE(ST_GEOMFROMTEXT(ST_ASTEXT(parkingzone.parkingzone_center_coord), 4326), ST_GEOMFROMTEXT(:p, 4326)) < parkingzone.parkingzone_radius)',
+        'isInParkingzone',
+      )
+      .addSelect(
+        'SUM(ST_Contains(forbidden_area.forbidden_area_boundary, ST_GeomFromText(:p)))',
+        'isInForbiddenArea',
+      )
+      .groupBy('history_id')
+      .setParameters({
+        p: `POINT (${latitude} ${longitude})`,
+      })
+      .execute();
+
+    const { isInArea, isInParkingzone, isInForbiddenArea } = historyQuery[0];
+    sendData = {
+      ...history,
+      useMin,
+      isInArea,
+      isInParkingzone,
+      isInForbiddenArea,
+    };
+    //TODO :: 이벤트 구현 후 주석 제거
+    // history.price = await this.eventSevice(sendData);
     try {
       return await this.save(history);
     } catch (error) {
       throw new InternalServerErrorException();
     }
-
-    //const { latitude, longitude } = updateHistroyDto;
-
-    //const historyQuery = this.createQueryBuilder('history')
-    //  .where('history_id = :id', { id })
-    //  .leftJoin('history.deer', 'deer')
-    //  .leftJoin('deer.area', 'area')
-    //  .leftJoinAndSelect('area.parkingzones', 'parkingzone')
-    //  .leftJoinAndSelect('area.forbidden_areas', 'forbidden_area')
-    //  .addSelect(
-    //    'ST_Contains(area.area_boundary, ST_GeomFromText(:p))',
-    //    'isInArea',
-    //  )
-    //  .addSelect(
-    //    'ST_DISTANCE(ST_GEOMFROMTEXT(ST_ASTEXT(parkingzone.parkingzone_center_coord), 4326), ST_GEOMFROMTEXT(:p, 4326)) < parkingzone.parkingzone_radius',
-    //    'isInParkingzone',
-    //  )
-    //  .addSelect(
-    //    'ST_Contains(forbidden_area.forbidden_area_boundary, ST_GeomFromText(:p))',
-    //    'isInForbiddenArea',
-    //  )
-    //  .setParameters({
-    //    p: `POINT (${latitude} ${longitude})`,
-    //  })
-    //  .execute();
-    //return historyQuery;
-
-    // const historyQuery = this.createQueryBuilder('history');
-
-    // const parkingArea = await historyQuery
-    //   .leftJoin('history.deer', 'deer')
-    //   .leftJoin('deer.area', 'area')
-    //   .where('ST_Contains(area.area_boundary, ST_GeomFromText(:p))', {
-    //     p: `POINT (${latitude} ${longitude})`,
-    //   })
-    //   .andWhere('history_id = :id', { id })
-    //   .execute();
-
-    // // area내에 있을때
-    // if (parkingArea) {
-    //   const parkingzone = await parkingArea
-    //     .leftJoinAndSelect('area.parkingzones', 'parkingzone')
-    //     .andWhere(
-    //       `ST_Distance(ST_GEOMFROMTEXT(ST_ASTEXT(parkingzone.parkingzone_center_coord), 4326), ST_GEOMFROMTEXT(:p, 4326), 'metre') < parkingzone.parkingzone_radius`,
-    //       {
-    //         p: `POINT (${latitude} ${longitude})`,
-    //       },
-    //     )
-    //     .andWhere('parkingzone.areaAreaId = area.area_id')
-    //     .execute();
-
-    //     // if(parkingzone) {
-    //     //   //할인
-    //     // } else if() {
-    //     //   //금지구역
-    //     // }
-    //     return parkingzone;
-    // } else {
-    //   //지역 얼마나 벗어났는지 계산
-    // }
-
-    // // return history;
   }
 
   async checkTransfer(user: User, history: History) {
