a63b84ca5e015f9ed775de617d0a622ca532c28d
doogler777 <doogler777@gmail.com>
2021-11-20 00:27:24 +0900

    feat: event전체 조회 추가
    - event.service에 eventGetList함수 추가
    resolves #24

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { HistoryService } from '../history/history.service';
 import { EventRepository } from './event.repository';
-import { DeerService } from '../deer/deer.service';
 import { CreateEventDto } from './dto/create-event.dto';
-import { User } from 'src/entities/user.entity';
-import { HistoryRepository } from 'src/history/history.repository';
-import { UserService } from 'src/user/user.service';
+import { User } from '../entities/user.entity';
+import { Event } from '../entities/event.entity';
 
 @Injectable()
 export class EventService {
   constructor(
     @InjectRepository(EventRepository)
     private eventRepository: EventRepository,
-    private deerService: DeerService,
-    private historyService: HistoryService,
-    // private historyRepository: HistoryRepository,
   ) {}
 
   async calculateEvents(user: User): Promise<any> {
     try {
-      const result = await this.findEvents();
+      // const result = await this.findEvents();
       // await this.historyService.calculateEvents(user);
     } catch (exception) {
       throw new Error(exception);
@@ -28,7 +22,11 @@ export class EventService {
     return null;
   }
 
-  async findEvents() {
+  async createEvent(createEventDto: CreateEventDto): Promise<Event> {
+    return await this.eventRepository.save({ ...createEventDto });
+  }
+
+  async eventGetList(): Promise<Event[]> {
     return await this.eventRepository.find();
   }
 }
