75fca7bd43b834f965803fdf87a9bef96bdbbc8a
lhj0621 <lhj0621@gmail.com>
2021-11-20 01:08:19 +0900

    feat: 이벤트 가격 계산 개발
    - 이벤트 가격 개산 API 개발
    
    resolves: #25

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -27,6 +27,27 @@ export class EventService {
   }
 
   async eventGetList(): Promise<Event[]> {
-    return await this.eventRepository.find();
+    return await this.eventRepository.find({
+      where: {
+        is_use: 1,
+      },
+      order: { plus_minus: 'DESC' },
+    });
+  }
+
+  async calculatePrice(data: any): Promise<number> {
+    const eventList = await this.eventGetList();
+    let price = data.price;
+    for (const el of eventList) {
+      const ifConditon = `data.${el.condition}`;
+      if (eval(ifConditon)) {
+        if (el.plus_minus == '할인') {
+          price = price * (1 - el.applied_number);
+        } else {
+          price = price + el.applied_number;
+        }
+      }
+    }
+    return price;
   }
 }
