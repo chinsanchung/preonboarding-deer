8ad496dda66a5a6f74acf34f2f04123ea44b6254
BangleCoding <effectivecodeyr@gmail.com>
2021-11-19 20:35:13 +0900

    feat: event findevents 기능 추가

--- src/event/event.repository.ts
+++ src/event/event.repository.ts
@@ -1,5 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Event } from '../entities/event.entity';
+import { CreateEventDto } from './dto/create-event.dto';
 
 @EntityRepository(Event)
 export class EventRepository extends Repository<Event> {}
--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -3,6 +3,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { HistoryService } from '../history/history.service';
 import { EventRepository } from './event.repository';
 import { DeerService } from '../deer/deer.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { User } from 'src/entities/user.entity';
+import { HistoryRepository } from 'src/history/history.repository';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class EventService {
@@ -11,9 +15,21 @@ export class EventService {
     private eventRepository: EventRepository,
     private deerService: DeerService,
     private historyService: HistoryService,
+    @InjectRepository(HistoryRepository)
+    private historyRepository: HistoryRepository,
   ) {}
 
-  findEvents() {
+  async calculateEvents(user: User): Promise<any> {
+    try {
+      const result = await this.findEvents();
+      await this.historyService.calculateEvents(user);
+    } catch (exception) {
+      throw new Error(exception);
+    }
     return null;
+  }
+
+  async findEvents() {
+    return await this.eventRepository.find();
   }
 }
