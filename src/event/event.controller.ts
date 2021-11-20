9c70a63dba7820d162cb3624027d6116d7018c74
Gibson Lee <gb.lee.gibson@gmail.com>
2021-11-20 16:08:56 +0900

    feat: 이벤트 수정
    - 이벤트의 is_use 값 수정 API 구현
    resolves: #30

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/auth-guard/jwt-auth.guard';
 import { CreateEventDto } from './dto/create-event.dto';
 import { EventService } from './event.service';
 import { Event } from '../entities/event.entity';
+import { UpdateEventDto } from './dto/update-event.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
@@ -14,8 +24,15 @@ export class EventController {
   }
 
   @Post()
-  @UseGuards(JwtAuthGuard)
   createEvent(@Body() createEventDto: CreateEventDto): Promise<Event> {
     return this.eventService.createEvent(createEventDto);
+  }
+
+  @Patch('/:id')
+  updateEvent(
+    @Param('id') id: string,
+    @Body() updateEventDto: UpdateEventDto,
+  ): Promise<Event> {
+    return this.eventService.updateEvent(Number(id), updateEventDto);
   }
 }
